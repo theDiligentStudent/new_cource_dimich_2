744893a0840aa7f86af899cf14f92b0c71f654f0
theDiligentStudent <ivanrenkin14@gmail.com>
2023-08-19 02:28:55 +0300

    Моя Сегодняшняя Переделка -- кода Димыча про Машину. ---- игрался с кнопкой Start и Статусом (состояния машниы)

--- js/app.js
+++ js/app.js
@@ -1,58 +1,41 @@
 // 11/08/23    Учу с Димычем его Новый курс "Переход на ООП"....
 //    т.к. я начал делать сам - Сразу через Об. и Ко.-р - у меня немного не вышло во Втором видео так, как у Димки
 //       Поэтому в отдельной папке я решил Всё Повторить За Ним
-// 12/08/23    
+// 12/08/23
 //       14/08/23 Lesson 3
+// 18/08/23    ----
+// долго /много дней/ НЕ занимался -  поэтому сейчас попробую повторить весь пройденный (который успел пройти) материал - по памяти
 
+let statusCar = document.querySelector('#car_status');
+let buttonStart = document.querySelector('#button_start');
 
+buttonStart.addEventListener('click', onButtonStartClick);
 
-// let buttonStart = document.querySelector('#button_click_1');
-// let buttonStart = document.querySelector('#button_click_2');
+function onButtonStartClick() {
+  let fiftyFifty = Math.random();
 
-
-
-
-function buttonStartClick() {
-  let randomChoice = Math.random();
-  if (randomChoice > 0.5) {
-    // Поменял Все alert-вызовы на console.log-и
-    console.log('The car started up. There is been a movement.');
-
+  if (fiftyFifty > 0.5) {
+    statusCar.innerHTML = 'Go!';
+    statusCar.classList.add('status-color-1');
     buttonStart.classList.add('hide');
-    statusCar1.innerHTML = 'Go!';
 
-    function carCrashed() {
-      console.log('Forced stop');
-      // buttonStart.classList.remove('hide');        Верну видимость Кнопке Start -- ниже...., когда статус 'Stop' сменится на 'off'
-    statusCar1.innerHTML = 'Stop';
-    statusCar1.classList.add('backgroundStatus');
-
+    function buttonDisappearing() {
+      buttonStart.classList.remove('hide');
+      statusCar.innerHTML = 'Engine Crashed!';
+      statusCar.classList.add('status-color-3');
+      buttonStart.innerHTML = 'STOP';
     }
-    window.setTimeout(carCrashed, 5000);
-
-    function againOff() {
-      statusCar1.innerHTML = 'off';
-// <<<<<<< HEAD
-    statusCar1.classList.remove('backgroundStatus');  
-    buttonStart.classList.remove('hide');        // Возвращаю видимость Кнопке Start
-// =======
-    statusCar1.classList.remove('backgroundStatus');
-
-// >>>>>>> 863b0a9 (Added status - when the car stops - Red color.)
-    }
-    window.setTimeout(againOff, 8000);
-
-    console.log('We wait Crash');
+    window.setTimeout(buttonDisappearing, 5000);
   } else {
-    console.log('Something went wrong.');
+    statusCar.innerHTML = 'Crash!';
+    statusCar.classList.add('status-color-4');
+    buttonStart.classList.add('hide');
+
+    function buttonDisappearing() {
+      buttonStart.classList.remove('hide');
+      statusCar.innerHTML = 'off';
+      statusCar.classList.remove('status-color-4');
+    }
+    window.setTimeout(buttonDisappearing, 2000);
   }
-
 }
-
-
-
-
-let buttonStart = document.querySelector('#button_click_1');
-let statusCar1 = document.querySelector('#status_car_1');
-
-buttonStart.addEventListener('click', buttonStartClick);
